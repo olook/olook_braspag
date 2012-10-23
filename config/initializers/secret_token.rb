f5941d4ed373258eeb1929177453a63408b6c293
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-10-23 15:16:22 -0200

    creating app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OlookBraspag::Application.config.secret_token = 'd7fab63f20368ad0635343569a9e24d225b8992c8da0bc8f5233cd66e5bd7f05dd01c0759992335b5c47fe5429841e2f4b3fa5ff7a2d8c6b6d241473f5782d2c'
